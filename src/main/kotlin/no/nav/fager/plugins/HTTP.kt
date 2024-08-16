9288716b9b01d848e6c9f0db47fbc708db0c85c2
Ken Gullaksen <ken.gullaksen@nav.no>
2024-08-16 13:40:18 +0200

    fjern openapi

--- src/main/kotlin/no/nav/fager/plugins/HTTP.kt
+++ src/main/kotlin/no/nav/fager/plugins/HTTP.kt
@@ -3,14 +3,8 @@ package no.nav.fager.plugins
 import io.ktor.server.application.*
 import io.ktor.server.plugins.compression.*
 import io.ktor.server.plugins.defaultheaders.*
-import io.ktor.server.plugins.openapi.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
 
 fun Application.configureHTTP() {
-    routing {
-        openAPI(path = "openapi")
-    }
     install(Compression) {
         gzip {
             priority = 1.0
