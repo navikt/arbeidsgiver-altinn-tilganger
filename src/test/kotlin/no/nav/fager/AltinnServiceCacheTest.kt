b89c5aa51bf098d653ebcf5708ce0f7471c6793d
Anders.Vik.Lysne <anders.vik.lysne@nav.no>
2024-09-19 11:19:58 +0200

    test av altinn cache hits

--- src/test/kotlin/no/nav/fager/AltinnServiceCacheTest.kt
+++ src/test/kotlin/no/nav/fager/AltinnServiceCacheTest.kt
@@ -1,33 +1,78 @@
 package no.nav.fager
 
 import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.mockk
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.EmptySource
-import org.junit.jupiter.params.provider.MethodSource
-import java.util.stream.Stream
+import kotlinx.coroutines.test.runTest
+import kotlin.test.Test
 
 class AltinnServiceCacheTest {
 
 
-    @ParameterizedTest
-    @MethodSource("testData")
-    fun `cacher altinn tilganger`(altinn2Tilgagner: Altinn2Tilganger, authorizedParties: List<AuthoririzedParty>, cacheGetCalls: Int, cacheSetCalls: Int){
+    @Test
+    fun `cache entry settes`() = runTest {
+        val redisMock = mockk<AltinnTilgangerRedisClient>()
+        coEvery { redisMock.get(any()) } returns null
+        coEvery { redisMock.set(any(), any()) } returns Unit
+
         val altinn2ClientMock = mockk<Altinn2Client>()
-        coEvery { altinn2ClientMock.hentAltinn2Tilganger(any()) } returns
+        coEvery { altinn2ClientMock.hentAltinn2Tilganger(any()) } returns Altinn2Tilganger(isError = false, orgNrTilTjenester = mapOf("910825496" to listOf(Altinn2Tjeneste(serviceCode = "4936", serviceEdition = "1"))))
+
+        val altinn3ClientMock = mockk<Altinn3Client>()
+        coEvery { altinn3ClientMock.hentAuthorizedParties(any()) } returns listOf(AuthoririzedParty(name = "SLEMMESTAD OG STAVERN REGNSKAP", organizationNumber = "910825496", authorizedResources = setOf("test-fager"), subunits = listOf(), unitType = "BEDR", type = "Business" ))
+
+        val altinnService = AltinnService(altinn2ClientMock, altinn3ClientMock, redisMock)
+
+        altinnService.hentTilganger("123", this)
+
+        coVerify(exactly = 1) {  redisMock.get(any()) }
+        coVerify(exactly = 1) {  redisMock.set(any(), any()) }
+        coVerify(exactly = 1) {  altinn2ClientMock.hentAltinn2Tilganger(any()) }
+        coVerify(exactly = 1) {  altinn3ClientMock.hentAuthorizedParties(any()) }
     }
 
+    @Test
+    fun `cache entry eksisterer`() = runTest {
+        val redisMock = mockk<AltinnTilgangerRedisClient>()
+        coEvery { redisMock.get(any()) } returns AltinnService.AltinnTilgangerResultat(isError = false, altinnTilganger = listOf(AltinnTilgang("910825496", altinn3Tilganger = setOf("test-fager"), altinn2Tilganger = setOf("4936:1"), underenheter = listOf(), name = "SLEMMESTAD OG STAVERN REGNSKAP", organizationForm = "BEDR")))
+        coEvery { redisMock.set(any(), any()) } returns Unit
 
-    companion object {
-        @JvmStatic
-        fun testData(): Stream<Arguments> {
-            return Stream.of(
-                Arguments.of(Altinn2Tilganger(isError = true, orgNrTilTjenester = mapOf())),
-                Arguments.of(),
-                Arguments.of(),
-            )
-        }
+        val altinn2ClientMock = mockk<Altinn2Client>()
+        coEvery { altinn2ClientMock.hentAltinn2Tilganger(any()) } returns Altinn2Tilganger(isError = false, orgNrTilTjenester = mapOf("910825496" to listOf(Altinn2Tjeneste(serviceCode = "4936", serviceEdition = "1"))))
+
+        val altinn3ClientMock = mockk<Altinn3Client>()
+        coEvery { altinn3ClientMock.hentAuthorizedParties(any()) } returns listOf(AuthoririzedParty(name = "SLEMMESTAD OG STAVERN REGNSKAP", organizationNumber = "910825496", authorizedResources = setOf("test-fager"), subunits = listOf(), unitType = "BEDR", type = "Business" ))
+
+        val altinnService = AltinnService(altinn2ClientMock, altinn3ClientMock, redisMock)
+
+        altinnService.hentTilganger("123", this)
+
+        coVerify(exactly = 1) {  redisMock.get(any()) }
+        coVerify(exactly = 0) {  redisMock.set(any(), any()) }
+        coVerify(exactly = 0) {  altinn2ClientMock.hentAltinn2Tilganger(any()) }
+        coVerify(exactly = 0) {  altinn3ClientMock.hentAuthorizedParties(any()) }
+    }
+
+    @Test
+    fun `cache entry settes ikke på grunn av feil i altinn2 respons`() = runTest {
+        val redisMock = mockk<AltinnTilgangerRedisClient>()
+        coEvery { redisMock.get(any()) } returns null
+        coEvery { redisMock.set(any(), any()) } returns Unit
+
+        val altinn2ClientMock = mockk<Altinn2Client>()
+        coEvery { altinn2ClientMock.hentAltinn2Tilganger(any()) } returns Altinn2Tilganger(isError = true, orgNrTilTjenester = mapOf("910825496" to listOf(Altinn2Tjeneste(serviceCode = "4936", serviceEdition = "1"))))
+
+        val altinn3ClientMock = mockk<Altinn3Client>()
+        coEvery { altinn3ClientMock.hentAuthorizedParties(any()) } returns listOf(AuthoririzedParty(name = "SLEMMESTAD OG STAVERN REGNSKAP", organizationNumber = "910825496", authorizedResources = setOf("test-fager"), subunits = listOf(), unitType = "BEDR", type = "Business" ))
+
+        val altinnService = AltinnService(altinn2ClientMock, altinn3ClientMock, redisMock)
+
+        altinnService.hentTilganger("123", this)
+
+        coVerify(exactly = 1) {  redisMock.get(any()) }
+        coVerify(exactly = 0) {  redisMock.set(any(), any()) }
+        coVerify(exactly = 1) {  altinn2ClientMock.hentAltinn2Tilganger(any()) }
+        coVerify(exactly = 1) {  altinn3ClientMock.hentAuthorizedParties(any()) }
     }
 }
 
