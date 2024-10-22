f78c5da3832c0898af487a800df6f626d2112d69
Anders.Vik.Lysne <anders.vik.lysne@nav.no>
2024-10-22 15:46:53 +0200

    kotlin syntax

--- src/main/kotlin/no/nav/fager/altinn/Altinn3TilAltinn2Map.kt
+++ src/main/kotlin/no/nav/fager/altinn/Altinn3TilAltinn2Map.kt
@@ -2,11 +2,9 @@ package no.nav.fager.altinn
 
 /*Mapper altinn ressurser til én eller flere gamle altinn 2 tilganger.*/
 class Altinn3TilAltinn2Tilganger {
-    companion object{
+    companion object {
         val MAP = mapOf(
-            Pair(
-                "nav_permittering-og-nedbemmaning_innsyn-i-alle-innsendte-skjemaer",
-                listOf(Altinn2Tjeneste("5810", "1"))
-            ))
+            "nav_permittering-og-nedbemmaning_innsyn-i-alle-innsendte-skjemaer" to listOf(Altinn2Tjeneste("5810", "1"))
+        )
     }
 }
