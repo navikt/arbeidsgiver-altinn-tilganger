358f66996a56b5d9f4bfad8868e8f4c2e1dd65cd
Ken Gullaksen <ken.gullaksen@nav.no>
2026-02-20 11:44:25 +0100

    remove password, not respected

--- src/main/kotlin/no/nav/fager/redis/Redis.kt
+++ src/main/kotlin/no/nav/fager/redis/Redis.kt
@@ -46,7 +46,7 @@ class RedisConfig(
                 /* host */ "127.0.0.1",
                 /* port */ 6379,
                 /* timeout */ 0,
-                /* password */ "123",
+                /* password */ null,
             )
         )
     }
