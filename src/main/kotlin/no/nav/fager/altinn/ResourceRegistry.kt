cf01af38137dd535d67b0889b0571aa69a65cce5
Ken Gullaksen <ken.gullaksen@nav.no>
2026-02-27 10:30:52 +0100

    nav_sykdom-i-familien_inntektsmelding tilgjengeglig i prod

--- src/main/kotlin/no/nav/fager/altinn/ResourceRegistry.kt
+++ src/main/kotlin/no/nav/fager/altinn/ResourceRegistry.kt
@@ -77,7 +77,6 @@ val KnownResources = listOfNotNull(
     Resource(
         resourceId = "nav_sykdom-i-familien_inntektsmelding",
         altinn2Tjeneste = listOf(Altinn2Tjeneste("4936", "1")),
-        availableInProduction = false, // Kan settes til true når ressursen finnes i altinn prod: https://platform.altinn.no/resourceregistry/api/v1/resource/nav_sykdom-i-familien_inntektsmelding
     ),
     Resource(
         resourceId = "nav_arbeidsforhold_aa-registeret-innsyn-arbeidsgiver",
